cc7d5a7cf5a0dd80fb323790724e457a369b3601
aNzTikTok <sshxvpn@proton.me>
2025-04-14 23:50:40 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,8 +6,16 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 # Set default theme to Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# Set default language to auto (remove forced language setting)
+# Set default language to English (en)
 sed -i '/config language/d' openwrt/package/lean/default-settings/files/zzz-default-settings
+echo 'uci set luci.main.lang=en' >> openwrt/package/lean/default-settings/files/zzz-default-settings
+echo 'uci commit luci' >> openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# Remove Chinese language files
+find openwrt/package -type f -name *zh_Hans* -delete
+find openwrt/feeds -type f -name *zh_Hans* -delete
+find openwrt/package -type f -name *zh-cn* -delete
+find openwrt/feeds -type f -name *zh-cn* -delete
 
 # Set default and permanent TTL to 64
 echo 'net.ipv4.ip_default_ttl=64' >> openwrt/package/base-files/files/etc/sysctl.conf
